59d63c894021ff0869dd02fc825ad7e6bc210c96
acapasso <69538713+acapasso90@users.noreply.github.com>
2021-07-02 14:21:05 -0400

    Added next/previous buttons and functionality
    Added next/previous buttons and functionality

--- src/Fifteen.js
+++ src/Fifteen.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import axios from "axios";
 import GameInfo from "./GameInfo";
 
@@ -6,8 +6,12 @@ export default function Fifteen(){
 const [loaded, setLoaded] = useState(false);
 const [arrayLength, setArrayLength] = useState();
 const [gameData, setGameData] = useState();
+const [maxPageLength, setMaxPageLength] = useState();
+const [page, setPage] = useState(0);
 
 function SetPrices(response){
+setMaxPageLength(response.headers["x-total-page-count"])
+console.log(response.data);
 setGameData(response.data);
 setArrayLength(response.data.length);
 setLoaded(true);
@@ -15,21 +19,58 @@ setLoaded(true);
 }
 
 function SearchPrices(){
-    let apiURL = `https://www.cheapshark.com/api/1.0/deals?upperPrice=15&page=1`;
+    let apiURL = `https://www.cheapshark.com/api/1.0/deals?upperPrice=15&pageSize=35&pageNumber=${page}`;
     axios.get(apiURL).then(SetPrices)
 
 }
 
+useEffect(() => {
+    let apiURL = `https://www.cheapshark.com/api/1.0/deals?upperPrice=15&pageSize=35&pageNumber=${page}`;
+    axios.get(apiURL).then(SetPrices)
+  }, [page]); 
+
+function nextPage(){
+setPage(page+1);
+}
+
+function prevPage(){
+    setPage(page-1);
+    }
+
 //First page of $15 games
-//https://www.cheapshark.com/api/1.0/deals?upperPrice=15&page=1
+//https://www.cheapshark.com/api/1.0/deals?upperPrice=15&pageNumber=1
 
 if (loaded){
+if (page === 0){
     return (
-        <div clasName="Fifteen">
+        <div className="Fifteen">
+            <h1> Games currently below $15</h1> 
+            <button onClick={nextPage}> Next Page</button>
+            {gameData.slice(0, arrayLength).map(function(gameNum, index){
+            return(<GameInfo data={gameNum} key={index} />)})}
+        </div>
+    )}
+else if (page > 0 && page < maxPageLength){
+    return (
+        <div className="Fifteen">
+            <h1> Games currently below $15</h1>
+           <button onClick={prevPage}>Previous Page</button> <button onClick={nextPage}> Next Page</button>
+
             {gameData.slice(0, arrayLength).map(function(gameNum, index){
             return(<GameInfo data={gameNum} key={index} />)})}
         </div>
     )
+}
+else {
+    return (
+        <div className="Fifteen">
+            <h1> Games currently below $15</h1>
+            <button onClick={prevPage}>Previous Page</button> 
+            {gameData.slice(0, arrayLength).map(function(gameNum, index){
+            return(<GameInfo data={gameNum} key={index} />)})}
+        </div>
+    )
+}
 }
    else { SearchPrices();
     return(
--- src/GameInfo.js
+++ src/GameInfo.js
@@ -1,7 +1,6 @@
-import React from "react";
+import React, {useEffect} from "react";
 
 export default function GameInfo(props){
-    console.log(props.data);
     const title = props.data.title;
     const thumb = props.data.thumb;
     const salePrice = props.data.salePrice;
@@ -11,6 +10,20 @@ export default function GameInfo(props){
     const steamNumberOfRatings = props.data.steamRatingCount;
     const steamRatingPercent = props.data.steamRatingPercent;
     const metaCriticRating = props.data.metacriticScore;
+
+    useEffect(() => {
+        const title = props.data.title;
+        const thumb = props.data.thumb;
+        const salePrice = props.data.salePrice;
+        const fullPrice = props.data.normalPrice;
+        const savings = Math.round(10*props.data.savings)/10;
+        const steamRatingSummary = props.data.steamRatingText;
+        const steamNumberOfRatings = props.data.steamRatingCount;
+        const steamRatingPercent = props.data.steamRatingPercent;
+        const metaCriticRating = props.data.metacriticScore;
+      }, [props]); 
+
+      
     return(
     <div className="GameInfo">
         <h3> {title} </h3>
