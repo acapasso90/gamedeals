590ed9626f3245730a71c74c07ae00442ab3cbcf
acapasso <69538713+acapasso90@users.noreply.github.com>
2021-07-06 15:07:08 -0400

    Reverted StoreData from state to variable. Added else statement
    Reverted StoreData from state to variable. Added else statement setting URL to null

--- src/StoreData.js
+++ src/StoreData.js
@@ -1,29 +1,31 @@
-import React, {useState} from "react";
+import React from "react";
 
 export default function StoreData(props){
-const [storeURL, setStoreURL] = useState(null);
+
 let storeID = props.data.storeID;
 let storeName = props.data.storeName;
 let singleStoreId = props.id;
 let steamID = props.steam;
 let formattedTitle = props.title.replace(":", "").replace(/__/g, "_");
+let storeURL = null;
 let dashedName = formattedTitle.replace(/_/g, '-').replace(":", "").replace("---", "-");
 let lowercaseName = dashedName.toLowerCase();
-if (storeName === "Steam") { setStoreURL(`https://store.steampowered.com/app/${steamID}/${formattedTitle}`); }
-else if (storeName === "Epic Games Store") {setStoreURL(`https://www.epicgames.com/store/en-US/p/${lowercaseName}`); }
+if (storeName === "Steam") { storeURL = `https://store.steampowered.com/app/${steamID}/${formattedTitle}`; }
+else if (storeName === "Epic Games Store") {storeURL = `https://www.epicgames.com/store/en-US/p/${lowercaseName}`; }
 else if (storeName === "Fanatical" ){
     if (formattedTitle.includes('Bundle')){
-        setStoreURL(`https://www.fanatical.com/en/bundle/${dashedName}`)}
-    else {setStoreURL(`https://www.fanatical.com/en/game/${dashedName}`)}; }
-else if (storeName === "GamersGate") { setStoreURL(`https://www.gamersgate.com/product/${lowercaseName}`);} 
+        storeURL = `https://www.fanatical.com/en/bundle/${dashedName}`}
+    else {storeURL = `https://www.fanatical.com/en/game/${dashedName}`}; }
+else if (storeName === "GamersGate") { storeURL = `https://www.gamersgate.com/product/${lowercaseName}`;} 
 
 else if (storeName === "GreenManGaming") {
-    setStoreURL(`https://www.greenmangaming.com/games/${dashedName}-pc`); }
-else if (storeName === "GOG") {setStoreURL(`https://www.gog.com/game/${formattedTitle}`);}
-else if (storeName === "Origin") {setStoreURL(`https://www.origin.com/usa/en-us/store/${dashedName}/${dashedName}`);}
-else if (storeName === "Humble Store") {setStoreURL(`https://www.humblebundle.com/store/${lowercaseName}`);}
-else if (storeName === "GameBillet") {setStoreURL(`https://www.gamebillet.com/${lowercaseName}`);}
-else if (storeName === "Voidu") {setStoreURL(`https://www.voidu.com/en/${lowercaseName}`);}
+    storeURL = `https://www.greenmangaming.com/games/${dashedName}-pc`; }
+else if (storeName === "GOG") {storeURL = `https://www.gog.com/game/${formattedTitle}`;}
+else if (storeName === "Origin") {storeURL = `https://www.origin.com/usa/en-us/store/${dashedName}/${dashedName}`;}
+else if (storeName === "Humble Store") {storeURL = `https://www.humblebundle.com/store/${lowercaseName}`;}
+else if (storeName === "GameBillet") {storeURL = `https://www.gamebillet.com/${lowercaseName}`;}
+else if (storeName === "Voidu") {storeURL = `https://www.voidu.com/en/${lowercaseName}` ;}
+else {storeURL = null;}
 
 if (storeID === singleStoreId){ 
     if (storeURL) {  return(<span className="StoreData">
