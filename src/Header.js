de23628dee2547d804815d046c3f978424347db5
acapasso <69538713+acapasso90@users.noreply.github.com>
2021-07-15 14:16:59 -0400

    Added hamburger dropdown on mobile, made header animation hover only on screen sizes larger than mobile sized media query
    Added hamburger dropdown on mobile, made header animation hover only on screen sizes larger than mobile sized media query

--- src/Header.js
+++ src/Header.js
@@ -1,31 +1,44 @@
 import React, {useEffect} from "react";
 import PiggyBank from "./media/piggybank.png";
 import { BrowserRouter as Router, Route, NavLink, Switch} from "react-router-dom";
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 
 export default function Header(){
 
+  const hamburger =  <i className="fas fa-bars" id="hamburgerIcon"></i>;
+  const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+
+console.log(vw);
   useEffect(() => {
     //Hover effect for Header 
     let header = document.querySelector('.headerContainer');
     let hidden = document.querySelector('.hidden');
-
-    header.addEventListener("mouseover", e => {
-      hidden.style.animation = '1.2s slide-in normal';
-      hidden.style.display = 'block';
-    });
-      header.addEventListener("mouseout", e => {
-        hidden.style.animation = '1.2s slide-out normal';
-        hidden.style.display = 'none';
-  }); }, [])
+    if(vw > 835){
+      header.addEventListener("mouseover", e => {
+        hidden.style.animation = '1.2s slide-in normal';
+        hidden.style.display = 'block';
+      });
+        header.addEventListener("mouseout", e => {
+          hidden.style.animation = '1.2s slide-out normal';
+          hidden.style.display = 'none';
+    })}; }, [])
 
 
 
     return(
   <div className="headerContainer">
       <div className="Header">
-                    <p className="headerText">GamerPiggy</p>
-                <img src={PiggyBank} alt="PiggyBank" className="piggyBank" /> 
+          <p className="headerText">GamerPiggy</p>
+          <img src={PiggyBank} alt="PiggyBank" className="piggyBank" />
+              <DropdownButton id='dropdown-button-drop-down-sort' className="hamburgerDropdown" title={hamburger}>
+                <div className="dropdownColumnHamburger">
+                  <NavLink to="/byfree" style={{ textDecoration: 'none' }} className="dropdownLink" > Free Games </NavLink> <br/>
+                  <NavLink to="/byprice" style={{ textDecoration: 'none' }} className="dropdownLink" id="dropdownLinkTwo" > Games By Price </NavLink> <br/>
+                  <NavLink to="/bystore" style={{ textDecoration: 'none' }} className="dropdownLink" id="dropdownLinkTwo" > Games By Store </NavLink> <br/>
+                  <NavLink to="/game" style={{ textDecoration: 'none' }} className="dropdownLink" id="dropdownLinkTwo" > Deals By Game </NavLink>
+                </div>
+              </DropdownButton>
         </div>
         <div className="hidden">
           <NavLink to="/byfree" className="headerLink">Free Games</NavLink>
